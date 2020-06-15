2d86537dc70c587989f4070eb08e4aa93eb6fd04
prasad <varaprasad1819@gmail.com>
2020-06-15 11:40:34 +0530

    new profile

---
+++ README.md
@@ -0,0 +1,3 @@
+# portfolio
+
+Profile View: https://gvvprasad.github.io/
---
+++ js/javascript.js
@@ -0,0 +1,41 @@
+/*TypeIt*/
+$(document).ready(function(){
+    $('.prointro').typeIt({
+        strings: ["Hi, I'm Prasad", "QA Engineer", "Automation (Selenium with Java) | API (Rest-Assured)"],
+        speed: 50,
+        autoStart: true
+    });
+});
+
+/*skills*/
+var chart = AmCharts.makeChart( "skill-chart", {
+    "type": "radar",
+    "theme": "dark",
+    "dataProvider": [ 
+      { "Technology": "Manual Testing",
+        "Percentage": 70  }, 
+      { "Technology": "Automation Testing",
+        "Percentage": 70  },
+      { "Technology": "API Testing",
+        "Percentage": 30  } 
+     ],
+  
+    "valueAxes": [ 
+      { "axisTitleOffset": 20,
+        "minimum": 0,
+        "axisAlpha": 0.15 } 
+    ],
+    
+    "startDuration": 2,
+    "graphs": [ 
+      { "balloonText": "[[value]]",
+        "bullet": "round",
+        "valueField": "Percentage" }
+    ],
+  
+    "categoryField": "Technology",
+    "export": {
+      "enabled": true
+    }
+  
+  });
