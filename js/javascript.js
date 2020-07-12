429640c55e00a5f197b3bf4870d5bc80bf824d34
prasad <varaprasad1819@gmail.com>
2020-07-12 10:35:00 +0530

    update

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 /*TypeIt*/
 $(document).ready(function(){
     $('.prointro').typeIt({
-        strings: ["Hi, I'm Prasad", "QA Engineer", "Automation (Selenium with Java) | API (Rest-Assured)"],
+        strings: ["Hi, I'm Prasad", "QA Engineer", "Automation (Selenium with Java) | API (Rest-Assured, Soap UI)"],
         speed: 50,
         autoStart: true
     });
